47f35f68cea1c7d0774d9d534a6e2451c5fa72d5
GuiMunizPB <guilhermemunizdeoliveira1@gmail.com>
2022-04-27 02:15:32 -0300

    Adiçao do projeto utilizando html css e js para os conceitos de java script assincrono

---
+++ assets/scripts.js
@@ -0,0 +1,24 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+
+
+
+const getCats = async () => {
+	try {
+		const data = await fetch(BASE_URL);
+		const json = await data.json();
+		return json.webpurl;
+	} catch (e) {
+		console.log(e.message);
+	}
+};
+
+const loadImg = async () => {
+	const img = document.getElementsByTagName('cat')[0];
+	img.src = await getCats();
+};
+
+const btn = document.getElementById('change-cat');
+btn.addEventListener('click', loadImg);
+
+loadImg();
+
